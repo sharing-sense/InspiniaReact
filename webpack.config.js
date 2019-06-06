8daef6a311220a14213ecbd0704092d6581eb292
sharing-sense <sharingsense@163.com>
2019-06-06 15:19:22 +0800

    update babel env

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = {
         loader: "babel-loader",
         exclude: [/node_modules/],
         query: {
-          presets: ["es2015", "react"],
+          presets: ["env", "react"],
           plugins: [
             ["transform-object-rest-spread"],
             ["import", { libraryName: "antd", style: true }]
